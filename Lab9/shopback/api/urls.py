7846bf3517f19c221895e7718ca9c9c26cd308e3
daniyarkyzydilyara <daniyarkyzydilyaraa@gmail.com>
2026-03-31 23:19:00 +0500

    Lab9

---
+++ Lab9/shopback/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import CategoryViewSet, ProductViewSet
+
+router = DefaultRouter()
+router.register(r"categories", CategoryViewSet, basename="categories")
+router.register(r"products", ProductViewSet, basename="products")
+
+urlpatterns = [
+    path("api/", include(router.urls)),
+]
